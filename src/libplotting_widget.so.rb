2c6eee4e26e48839a348ec62918dc3f549f800c3
Alexander Duda <Alexander.Duda@dfki.de>
2010-10-18 10:44:41 +0200

    updated rb file

--- src/libplotting_widget.so.rb
+++ src/libplotting_widget.so.rb
@@ -1,21 +1,22 @@
 #prepares the c++ qt widget for the use in ruby with widget_grid
-  
-  def __plugin_widget__.types
+
+WidgetFactory::extend_widget do
+  def types
     return []
   end
 
-  def __plugin_widget__.default_options()
+  def default_options
       options = Hash.new
       options[:min_width] = 140
       options[:min_height] = 140
       return options
   end
 
-  def __plugin_widget__.default_display_widget
+  def default_display_widget
     types
   end
 
-  def __plugin_widget__.config(options= Hash.new)
+  def config(options= Hash.new)
     @options ||= default_options()
     @options = @options.merge(options)
     return @options
@@ -23,6 +24,7 @@
 
   #diplay is called each time new data are available on the orocos output port
   #this functions translates the orocos data struct to the widget specific format
-  def __plugin_widget__.display(port,frame)
+  def display(port,frame)
     
   end
+end
