5c4b464aafb31858285872653622ae02bb43ebc4
Alexander <aduda@aduda-l.localdomain>
2011-10-04 08:16:42 +0200

    prototype sonar beam

--- src/vtk/sonar_display/SonarDisplay.cc
+++ src/vtk/sonar_display/SonarDisplay.cc
@@ -12,8 +12,12 @@
 #include <vtkInteractorStyleImage.h>
 #include <vtkRenderer.h>
 #include <vtkJPEGReader.h>
+#include <vtkLookupTable.h>
+
+#include <vtkCellData.h>
 
 #include <vtkVolumeRayCastCompositeFunction.h>
+#include <vtkUnstructuredGridVolumeRayCastMapper.h>
 #include <vtkVolumeRayCastMapper.h>
 #include <vtkMath.h>
 #include <vtkDoubleArray.h>
@@ -22,6 +26,7 @@
 #include <vtkXYPlotActor.h>
 
 #include <vtkStructuredGrid.h>
+#include <vtkDataSetTriangleFilter.h>
 #include <vtkMath.h>
 #include <vtkHedgeHog.h>
 #include <vtkContourFilter.h>
@@ -34,7 +39,7 @@
 #include <vtkVolumeProperty.h>
 #include <vtkPiecewiseFunction.h>
 #include <vtkColorTransferFunction.h>
-
+#include <vtkUnstructuredGridBunykRayCastFunction.h>
 #include <vtkFloatArray.h>
 #include "vtkPointData.h"
 #include "vtkPoints.h"
@@ -50,11 +55,11 @@ SonarDisplay::SonarDisplay(QWidget *parent):
 {
     this->resize(256,256);
 
-    const int number_of_bins = 100;
-    const int number_of_beams = 121;
-    const float distance_resolution = 0.1;
+    const int number_of_bins = 5;
+    const int number_of_beams = 31;
+    const float distance_resolution = 2;
     const float horizontal_angular_resolution = 30 * vtkMath::DegreesToRadians();
-    const float vertical_angular_resolution = 3.0* vtkMath::DegreesToRadians();
+    const float vertical_angular_resolution = 12.0* vtkMath::DegreesToRadians();
 
     // Create the structured grid.
     vtkStructuredPoints *spoints = vtkStructuredPoints::New();
@@ -62,6 +67,8 @@ SonarDisplay::SonarDisplay(QWidget *parent):
     vtkStructuredGrid *sgrid = vtkStructuredGrid::New();
     sgrid->SetDimensions(number_of_beams,number_of_bins,2);
     spoints->SetDimensions(number_of_beams,number_of_bins,2);
+
+    vtkFloatArray *array = vtkFloatArray::New();
 
     vtkPoints *points = vtkPoints::New();
     points->Allocate(number_of_bins*number_of_beams*2);
@@ -82,72 +89,86 @@ SonarDisplay::SonarDisplay(QWidget *parent):
                 point[0] = radius * cos(theta);
                 point[1] = radius * sin(theta);
                 points->InsertNextPoint(point);
+                array->InsertNextValue(i+rand()%100);
             }
-            spoints->SetScalarComponentFromFloat(10,j,k,0,200);
+           // spoints->SetScalarComponentFromFloat(10,j,k,0,200);
         }
     }
     sgrid->SetPoints(points);
-    
-    //sgrid->GetPointData()->SetVectors(vectors);
-    //vectors->Delete();
-
-
-
+    sgrid->GetPointData()->SetScalars(array);
 
     // We create a simple pipeline to display the data.
     vtkDataSetMapper *isoMapper = vtkDataSetMapper::New();
+    vtkDataSetMapper *isoMapper2 = vtkDataSetMapper::New();
     isoMapper->SetInput(sgrid);
-    isoMapper->ScalarVisibilityOff();
+    isoMapper2->SetInput(sgrid);
+    isoMapper2->ScalarVisibilityOff();
 
-    //   vtkPolyDataMapper *sgridMapper = vtkPolyDataMapper::New();
-    //   sgridMapper->SetInputConnection(isoMapper->GetOutputPort());
+     vtkLookupTable *lut = vtkLookupTable::New();
+            lut->SetNumberOfColors(20);
+            lut->SetHueRange(0.0,1.0);
+            lut->Build();
+    isoMapper->SetLookupTable(lut);
+    isoMapper->SetScalarRange(0,255);
+
+    //isoMapper->ScalarVisibilityOff();
+
     vtkActor *sgridActor = vtkActor::New();
-    //   sgridActor->SetMapper(sgridMapper);
+    vtkActor *sgridActor2 = vtkActor::New();
     sgridActor->SetMapper(isoMapper);
-    sgridActor->GetProperty()->SetColor(0,0,0);
-    sgridActor->GetProperty()->SetRepresentationToWireframe();
+    sgridActor2->SetMapper(isoMapper2);
+    sgridActor2->GetProperty()->SetColor(0,0,0);
+    sgridActor->GetProperty()->SetOpacity(0.9);
+    sgridActor2->GetProperty()->SetRepresentationToWireframe();
 
     vtkVolumeProperty *volumeProperty = vtkVolumeProperty::New();
     volumeProperty->ShadeOff();
     volumeProperty->SetInterpolationType(VTK_LINEAR_INTERPOLATION);
 
-    vtkSmartPointer<vtkPiecewiseFunction> compositeOpacity = vtkSmartPointer<vtkPiecewiseFunction>::New();
-    compositeOpacity->AddPoint(0.0,0.0);
-    compositeOpacity->AddPoint(80.0,1.0);
-    compositeOpacity->AddPoint(80.1,0.0);
-    compositeOpacity->AddPoint(255.0,0.0);
+    /*vtkSmartPointer<vtkPiecewiseFunction> compositeOpacity = vtkSmartPointer<vtkPiecewiseFunction>::New();
+    compositeOpacity->AddPoint(0.0,0.8);
+    compositeOpacity->AddPoint(80.0,0.8);
+    compositeOpacity->AddPoint(80.1,0.8);
+    compositeOpacity->AddPoint(255.0,0.8);
     volumeProperty->SetScalarOpacity(compositeOpacity); // composite first.
 
     vtkSmartPointer<vtkColorTransferFunction> color = vtkSmartPointer<vtkColorTransferFunction>::New();
     color->AddRGBPoint(0.0  ,0.0,0.0,1.0);
-    color->AddRGBPoint(40.0  ,1.0,0.0,0.0);
+    color->AddRGBPoint(41.0  ,1.0,0.0,0.0);
+    color->AddRGBPoint(100.0  ,1.0,1.0,0.0);
     color->AddRGBPoint(255.0,1.0,1.0,1.0);
     volumeProperty->SetColor(color);
 
-    vtkVolumeRayCastCompositeFunction *compositeFunction = vtkVolumeRayCastCompositeFunction::New();
-    vtkVolumeRayCastMapper *volumeMapper = vtkVolumeRayCastMapper::New();
+    //vtkVolumeRayCastCompositeFunction *compositeFunction = vtkVolumeRayCastCompositeFunction::New();
+    //vtkVolumeRayCastMapper *volumeMapper = vtkVolumeRayCastMapper::New();
+    //volumeMapper->SetVolumeRayCastFunction(compositeFunction);
 
-    volumeMapper->SetVolumeRayCastFunction(compositeFunction);
-    volumeMapper->SetInput(sgrid);
+    vtkUnstructuredGridBunykRayCastFunction* func = vtkUnstructuredGridBunykRayCastFunction::New();
+    vtkUnstructuredGridVolumeRayCastMapper *volumeMapper = vtkUnstructuredGridVolumeRayCastMapper::New();
+    volumeMapper->SetRayCastFunction(func);
+
+    vtkDataSetTriangleFilter* filter2 = vtkDataSetTriangleFilter::New();
+    filter2->SetInput(sgrid);
+    volumeMapper->SetInputConnection(filter2->GetOutputPort());
 
     vtkSmartPointer<vtkVolume> volume = vtkSmartPointer<vtkVolume>::New();
     volume->SetMapper(volumeMapper);
-    volume->SetProperty(volumeProperty);
+    volume->SetProperty(volumeProperty);*/
 
     //setup window
-    vtkSmartPointer<vtkRenderWindow> renderWindow = 
-        vtkSmartPointer<vtkRenderWindow>::New();
+    vtkSmartPointer<vtkRenderWindow> renderWindow = vtkSmartPointer<vtkRenderWindow>::New();
 
     //setup renderer
     vtkSmartPointer<vtkRenderer> renderer = vtkSmartPointer<vtkRenderer>::New();
     renderWindow->AddRenderer(renderer);
 
+    renderer->AddActor(sgridActor2);
     renderer->AddActor(sgridActor);
     renderer->ResetCamera();
 
-    renderer->AddVolume(volume);
+    //renderer->AddVolume(volume);
 
-    renderer->SetBackground(1,1,1);
+    renderer->SetBackground(0.1,0.1,0.1);
     renderer->GetActiveCamera()->Elevation(60.0);
     renderer->GetActiveCamera()->Azimuth(30.0);
     renderer->GetActiveCamera()->Zoom(1.25);
