09e2721b85bc02c4a9bd9d3be0caf2032b7e172b
Alexander Duda <Alexander.Duda@dfki.de>
2011-10-04 21:24:50 +0200

    add test script

---
+++ test.rb
@@ -0,0 +1,16 @@
+require 'vizkit'
+require 'orocos/log'
+
+
+log = Orocos::Log::Replay.open(ARGV)
+widget = Vizkit.default_loader.SonarDisplay
+
+
+log.sonar_rear.BaseScan.connect_to do |data,_|
+    widget.addSonarBeam(data.bearing.rad+Math::PI,data.beam.size-8,data.beam.to_byte_array[8..-1])
+end
+
+widget.show
+
+Vizkit.control log
+Vizkit.exec
