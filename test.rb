4f95af252ce2991c05e228c732f87edad5256b1c
Alexander Duda <Alexander.Duda@dfki.de>
2011-10-28 18:57:29 +0200

    add some tests

--- test.rb
+++ test.rb
@@ -3,9 +3,14 @@ require 'orocos/log'
 
 
 log = Orocos::Log::Replay.open(ARGV)
+sonar_data = log.port_for("base::samples::SonarBeam")
 widget = Vizkit.default_loader.SonarDisplay
 
-log.sonar_rear.BaseScan.connect_to do |data,_|
+def test(data,port)
+    puts port
+end
+
+sonar_data.connect_to method(:test) do |data,_|
     widget.addSonarBeam(data.bearing.rad+Math::PI,data.beam.size-8,data.beam.to_byte_array[8..-1])
 end
 
--- test2.rb
+++ test2.rb
@@ -1,14 +1,16 @@
 require 'vizkit'
 require 'orocos/log'
+Orocos.initialize
 
 
 require 'octave'
 engine = Octave::Engine.new
 
 log = Orocos::Log::Replay.open(ARGV)
+sonar_data = log.find_port("/base/samples/SonarBeam")
 widget = Vizkit.default_loader.SonarDisplay
 
-log.sonar_rear.BaseScan.connect_to do |data,_|
+sonar_data.connect_to do |data,_|
   a = engine.mytest(to_octave(data))
   from_octave(data,a)
   widget.addSonarBeam(data.bearing.rad+Math::PI,data.beam.size-8,data.beam.to_byte_array[8..-1])
@@ -16,6 +18,9 @@ log.sonar_rear.BaseScan.connect_to do |data,_|
 end
 
 widget.show
+
+#load video stream
+Vizkit.display log.bottom_camera.frame
 
 Vizkit.control log
 Vizkit.exec
