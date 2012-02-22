0a703ff770140b0eabde3cad9194904f8ddbd1c1
Allan E. Conquest <allan.conquest@dfki.de>
2012-02-22 10:07:26 +0100

    renamed index slider image

--- src/timeline/IndexSlider.cc
+++ src/timeline/IndexSlider.cc
@@ -8,8 +8,8 @@ IndexSlider::IndexSlider() {
 
 IndexSlider::IndexSlider(QGraphicsItem* parent, unsigned initIdx) : Slider(parent, initIdx) {
     setLastIndex(((SlideBarItem*)parentItem())->getStartIndex()); // initialize variable to first index for safety reasons.
-    setShapeMode(BoundingRectShape);
-    setPixmap(QPixmap(":/timeline/test_index_slider"));
+    //setShapeMode(BoundingRectShape);
+    setPixmap(QPixmap(":/timeline/index_slider"));
     setOffset(-0.5*width(), -0.5*height());
 }
 
