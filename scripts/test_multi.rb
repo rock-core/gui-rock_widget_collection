2958ba4956dfb0388f60fdc2234863c3418d7a11
Allan E. Conquest <allan.conquest@dfki.de>
2012-08-22 17:36:29 +0200

    removed unnecessary script

--- scripts/test_multi.rb
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env ruby
-
-require 'vizkit'
-# change the path to a correct one
-multi = Vizkit.load("/home/blueck/temp/test/tryit.ui")
-#multi.multiViewWidget.setDesignerMode(false)
-
-multi.show
-Vizkit.exec
